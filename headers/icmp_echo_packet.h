6fa3928115fcc82d3e2d1d029ae246a38c5b19bf
Seabass Jolynn <sjolynn@sjolynn.home>
2024-04-01 19:50:28 +0200

    Send ping, receive ping

--- headers/icmp_echo_packet.h
+++ headers/icmp_echo_packet.h
@@ -4,11 +4,11 @@
 
 struct s_header
 {
-    unsigned char type;
-    unsigned char code;
-    short checksum;
-    short identifier;
-    short sequence_number;
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
+    uint16_t identifier;
+    uint16_t sequence_number;
 };
 
 struct s_icmp_echo_request_packet
--- src/main.c
+++ src/main.c
@@ -50,26 +50,35 @@ struct s_icmp_echo_request_packet create_icmp_echo_request_packet()
     header.code = 0;
     header.checksum = 0;
     header.identifier = getpid();
-
+    header.sequence_number = 0;
+    
     packet.header = header;
-    memset(&packet.payload, 13, sizeof(packet.payload));
+    memset(&packet.payload, 'P', sizeof(packet.payload));
     return packet;
 }
 
-unsigned short caclulate_checksum(void *b, int len) 
+uint16_t caclulate_checksum(void *addr, int len) 
 {    
-    unsigned short *buf = b;
-    unsigned int sum = 0;
-    unsigned short result;
+    uint32_t sum = 0;
+    while (len > 1)
+    {
+        sum += *((uint16_t *) addr);  
+        addr += 2;
+        len -= 2;
+    }
 
-    for (sum = 0; len > 1; len -= 2)
-        sum += *buf++;
-    if (len == 1)
-        sum += *(unsigned char *)buf;
-    sum = (sum >> 16) + (sum & 0xFFFF);
-    sum += (sum >> 16);
-    result = ~sum;
-    return result;
+    if( len > 0 )
+    {
+        sum += * (uint8_t*) addr;
+    }
+               
+
+    /*  Fold 32-bit sum to 16 bits */
+    while (sum >> 16)
+    {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+    return ~sum;
 }
 
 #define TIME_INTERVAL_BETWEEN_PINGS_SEC 1
@@ -86,27 +95,27 @@ int main(int ac, char **av) {
     hints.ai_socktype = SOCK_RAW; // Raw socket for ICMP
     hints.ai_protocol = IPPROTO_ICMP; // ICMP protocol
 
-    struct addrinfo *resloved_address;
+    struct addrinfo *resolved_address;
     
-    int result = getaddrinfo(av[1], NULL, &hints, &resloved_address);
+    int result = getaddrinfo(av[1], NULL, &hints, &resolved_address);
     if (result != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result)); //real ping prints: ping: host unknown in case of blabla host
-        freeaddrinfo(resloved_address);
+        freeaddrinfo(resolved_address);
         exit(EXIT_ERROR_CODE);
     }
 
 
     //here i cast struct sockaddr* to struct sockaddr_in* because ai_addr may contain different address structures: struct sockaddr_in* - for ipv4, struct sockaddr_in6* for ipv6
-    struct sockaddr_in remote_host_net_addr = *((struct sockaddr_in*) resloved_address->ai_addr);
+    struct sockaddr_in remote_host_net_addr = *((struct sockaddr_in*) resolved_address->ai_addr);
     char remote_host_str_addr[INET_ADDRSTRLEN];
     bzero(remote_host_str_addr, INET_ADDRSTRLEN);
     
     if(inet_ntop(AF_INET,&(remote_host_net_addr.sin_addr), remote_host_str_addr, INET_ADDRSTRLEN) == NULL) {
         fprintf(stderr, "inet_ntop: %s\n", strerror(errno));
-        freeaddrinfo(resloved_address);
+        freeaddrinfo(resolved_address);
         exit(EXIT_ERROR_CODE);
     }
-    freeaddrinfo(resloved_address);
+    freeaddrinfo(resolved_address);
     
     //---------------------open socket
     int socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
@@ -115,18 +124,51 @@ int main(int ac, char **av) {
         exit(EXIT_ERROR_CODE);
     }
     
+    struct timeval tv_out;
+    tv_out.tv_sec = 1;
+    tv_out.tv_usec = 0;
+    setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof tv_out);
+
     //----------------------create packet;
-    struct s_icmp_echo_request_packet packet = create_icmp_echo_request_packet();
+    int sequence_number = 0;
     while (true)
     {
         sleep(TIME_INTERVAL_BETWEEN_PINGS_SEC);
-        packet.header.sequence_number = packet_sequence_number++;
+        struct s_icmp_echo_request_packet packet = create_icmp_echo_request_packet();
+        packet.header.sequence_number = sequence_number++;
         packet.header.checksum = caclulate_checksum(&packet, PACKET_LENGTH);
+        
         //-------------------- print ping header
         printf("PING %s (%s): %d data bytes\n", av[1], remote_host_str_addr, PING_PACKET_SIZE);
+        int sendResult = sendto(socket_fd, &packet, PACKET_LENGTH, 0, resolved_address->ai_addr, resolved_address->ai_addrlen);
+        if (sendResult == -1) 
+        {
+            printf("Error when sending ICPM packet. Error: %s\n", strerror(errno));
+            //need cleanup function to free address and socket
+            exit(EXIT_ERROR_CODE);
+        } else {
+            printf("Send ICPM packet successfuly. Number of bytes sent is: %d\n", sendResult);
+        }
+        
+        uint8_t receivedPacket[400];
+        bzero(receivedPacket, 400);
+        int recieveResult = recvfrom(socket_fd, receivedPacket, 400, 0, NULL, 0);
+        if (recieveResult != -1)
+        {
+            //printf("Received result: %d\n", recieveResult);
+            //int ip_header_len = receivedPacket[0] & 0xf;
+            //printf("Ip header length: %d\n", ip_header_len);
+            struct s_icmp_echo_request_packet r = *((struct s_icmp_echo_request_packet *) (receivedPacket + 20));
+            printf("Received ICMP reply. Id: %d sequence num: %d\n", r.header.identifier, r.header.sequence_number);
+        }
+        else
+        {
+            printf("Failed to receive ICPM packet");
+        }
     }
 
-
     printf("Socket fd %d\n", socket_fd);
+    //close socket?
+    //freeaddrinfo(resolved_address);
     return 0;
 }
